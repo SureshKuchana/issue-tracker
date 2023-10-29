25c2b758c55a77d9caaafbfeb2ba6a5c8fb38977
SureshKuchana <kuchana123.suresh@gmail.com>
2023-10-29 12:06:26 +0530

    fixed missing file change

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/client';
-import { createIssueSchema } from './createIssueSchema';
+import { createIssueSchema } from '../../validationSchemas';
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
