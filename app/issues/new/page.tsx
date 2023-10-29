48abde457a961befb2d20cab9a9cb35ef640a268
SureshKuchana <kuchana123.suresh@gmail.com>
2023-10-29 12:22:47 +0530

    refactor move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -28,6 +28,19 @@ const NewIssuePage = () => {
   const [error, setError] = useState('');
   const [isSubmitting, setSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setSubmitting(false);
+      // eslint-disable-next-line no-console
+      console.log('[Error] while submitting the form ', error);
+      setError('An unexpected error occurred.');
+    }
+  });
+
   return (
     <div className="max-w-xl">
       {error && (
@@ -35,21 +48,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-          } catch (error) {
-            setSubmitting(false);
-            // eslint-disable-next-line no-console
-            console.log('[Error] while submitting the form ', error);
-            setError('An unexpected error occurred.');
-          }
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register('title')} />
         </TextField.Root>
