75f781d50cb8eaa8c183e5312e15ee832bdaab34
SureshKuchana <kuchana123.suresh@gmail.com>
2024-07-21 14:09:54 +0530

    add dashboard & issue summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
-import Pagination from './components/Pagination';
+import prisma from '@/prisma/client';
+import IssueSummary from './IssueSummary';
+import LatestIssues from './LatestIssues';
+import IssueChart from './IssueChart';
+import { Flex, Grid } from '@radix-ui/themes';
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: 'OPEN' } });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
   return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
+    <>
+      <Grid columns={{ initial: '1', md: '2' }} gap="5">
+        <Flex direction={'column'} gap="5">
+          <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+          <IssueChart open={open} inProgress={inProgress} closed={closed} />
+        </Flex>
+        <LatestIssues />
+      </Grid>
+    </>
   );
 }
